331972b574d43449f7fc30243b74d5b450c1532c
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-08 10:44:02 -0500

    Create Error404500ApplicationTests.java

---
+++ src/test/java/com/example/error404500/Error404500ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.error404500;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Error404500ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
