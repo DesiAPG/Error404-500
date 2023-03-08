cb2291afe1c3595ebb55b0705022ff64d410c85b
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-08 10:43:54 -0500

    Create Error404500Application.java

---
+++ src/main/java/com/example/error404500/Error404500Application.java
@@ -0,0 +1,13 @@
+package com.example.error404500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Error404500Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Error404500Application.class, args);
+    }
+
+}
